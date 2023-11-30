4f451ef725358205487b40e79ee9f5c3774684ad
BenasB <29711974+BenasB@users.noreply.github.com>
2023-12-01 01:16:46 +0200

    Add services

--- Entities/Service.cs
+++ Entities/Service.cs
@@ -1,5 +1,33 @@
 using System.ComponentModel.DataAnnotations;
 
+record ServiceInformation
+{
+    /// <example>Children (up to 14) haircut</example>
+    [Required]
+    public string Name { get; set; }
+
+    /// <example>3.99</example>
+    [Required]
+    [DataType(DataType.Currency)]
+    public double Price { get; set; }
+
+    /// <example>Our skilled stylists create a welcoming atmosphere, ensuring a comfortable and enjoyable grooming session. With attention to detail and a playful touch, we craft trendy and age-appropriate hairstyles, making every visit a delightful adventure for your child.</example>
+    [Required]
+    public string Description { get; set; }
+}
+
+record Service : ServiceInformation
+{
+    /// <example>43</example>
+    [Required]
+    public int Id { get; set; }
+
+    /// <example>0.84</example>
+    [Required]
+    [DataType(DataType.Currency)]
+    public double Tax { get; set; }
+}
+
 record AppointmentInformation
 {
     /// <example>43</example>
